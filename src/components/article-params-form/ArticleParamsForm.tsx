ce199bd1fe2b71d467a99aff313a48fac10a825b
Daria Sokolova <dariasokolova@192.168.1.4>
2025-05-06 12:38:22 +0300

    версия2

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,39 @@
+import { useState } from 'react';
 import { ArrowButton } from 'src/components/arrow-button';
 import { Button } from 'src/components/button';
-
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const handleToggle = () => {
+		setIsOpen((prev) => !prev);
+	};
+
+	const handleReset = () => {
+		// Сбросить все значения формы
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		// Отправить выбранные значения вверх
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpen} onClick={handleToggle} />
+			{isOpen && (
+				<aside className={styles.container}>
+					<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
+						{/* Здесь будут опции формы, например <RadioGroup />, <Select />, ... */}
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
+
